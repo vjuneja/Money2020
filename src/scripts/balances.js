42ff6b6b1663008c32552addd51f2942fe6c3656
Liang <shliang@ebay.com>
2018-10-20 14:35:19 -0700

    Add manual balances

--- src/scripts/balances.js
+++ src/scripts/balances.js
@@ -2,8 +2,20 @@
 
 function getTotalBalance(fromDate, toDate, accountId) {
     // Call service
-
-    // Aggregate into total by date
     
+    // Aggregate into total by date
 
+}
+
+function addManualTransaction(date, amount, interval) {
+    var currentBalance = getTotalBalance();
+    currentBalance.forEach(ele => {
+        // We really shouldn't have values smaller than days
+        var dateDiffInDays = Math.round((new Date(date) - new Date(ele.date))) / 1000 / 60 / 60 / 24;
+        if (dateDiffInDays % interval === 0) {
+            // Pass in negative value if it's debit
+            ele.balance.amount += amount;
+        }
+    });
+    return currentBalance;
 }
