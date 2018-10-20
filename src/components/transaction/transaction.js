d2681b1f0ec1dfdc644075e1724036c7fdf80240
Om Prakash Kannusami <okannusami@ebay.com>
2018-10-20 15:25:41 -0700

    adding trasaction and header

---
+++ src/components/transaction/transaction.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import Header from '../header'
+
+
+export default class Transaction extends React.Component {
+
+
+    render(){
+        return (
+            <div>
+                <Header />
+                Add Transaction
+            </div>)
+    }
+}
