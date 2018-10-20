d4e3992d46bbffed3d8c1c39fc990cc10a62acc2
paulgoblin <mpr75@cornell.edu>
2018-10-20 14:45:44 -0700

    Add FE router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from "react-dom"
+import Router from './components/router'
 import HomePage from './components/home-page'
 
+const Test = () => <div>test</div>
+
+const router = (
+    <Router
+        paths={{
+            'test': Test,
+            'default': HomePage
+        }}
+    />
+)
+
 const wrapper = document.getElementById("app");
-wrapper ? ReactDOM.render(<HomePage test="testprop"/>, wrapper) : false;
+wrapper ? ReactDOM.render(router, wrapper) : false;
