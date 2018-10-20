28c6136d13d2e9907d1ea30f98a16c6a0ea08ff4
Om Prakash Kannusami <okannusami@ebay.com>
2018-10-20 16:49:33 -0700

    adding state to transaction

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,7 +6,7 @@ const Header = ()=> {
         <table width="100%">
             <tr style={{"vertical-align": "middle"}}>
             <td className="mui--appbar-height" style={{"font-size": "18px", "font-weight": "bold"}}>Equalizer</td>
-            <td className="mui--appbar-height" align="right">Right Side</td>
+            <td className="mui--appbar-height" align="right"><i className="fas fa-home" style={{"font-size": "32px"}}/></td>
             </tr>
         </table>
     </div>
--- src/components/transaction/transaction.js
+++ src/components/transaction/transaction.js
@@ -3,7 +3,60 @@ import React from 'react'
 import Header from '../header'
 
 
+
+const EMI = ()=> {
+    return (
+        <div>
+            <legend>Provide EMI details</legend>
+            <div class="mui-textfield">
+                <input type="text" placeHolder="Amount"/>
+                <label>EMI Amount</label>
+            </div>
+            <div class="mui-textfield">
+                <input type="text" placeHolder="Months"/>
+                <label>Number of months</label>
+            </div>
+        </div>
+    )
+}
+
+const Accounts = () => {
+    return (
+        <div class="mui-select">
+            <select>
+                <option>Bank of America - Checking</option>
+                <option>Chase Saphire Reserve - 5544</option>
+                <option>Bank of America Cash Rewards Visa Signature - 6112</option>
+                <option>Bank of America Travel Rewards Visa Signature - 6112</option>
+                </select>
+            <label>Select a bank account</label>
+        </div>
+    )
+}
+
+const OTP = () => {
+    return (
+        <div class="mui-textfield">
+            <input type="text" placeHolder="Final amount"/>
+            <label>Enter final payment amount</label>
+        </div>
+    )
+}
+
 export default class Transaction extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            txnType: null
+        }
+    }
+
+    selectTranaction(e) {
+        this.setState({
+            txnType: e.currentTarget.dataset["txnType"]
+        })
+    }
 
 
     render(){
@@ -14,36 +67,20 @@ export default class Transaction extends React.Component {
                 <div style={{"font-weight": "bold", "font-size": "16px", "marginBottom": ".5rem"}}>Add a transaction</div>
                     <div class="mui-dropdown" style={{"display": "block"}}>
                         <button class="mui-btn mui-btn--primary" data-mui-toggle="dropdown" style={{"width": "100%"}}>
-                            Type of payment
+                             {this.state.txnType ? this.state.txnType: `Type of payment`} 
                             <span class="mui-caret"></span>
                         </button>
                         <ul class="mui-dropdown__menu" style={{"width": "100%"}}>
-                            <li><a href="javascript:;">EMI</a></li>
-                            <li><a href="javascript:;">One Time Payment</a></li>
+                            <li><a href="javascript:;" onClick={this.selectTranaction.bind(this)} data-txn-type="EMI">EMI</a></li>
+                            <li><a href="javascript:;" onClick={this.selectTranaction.bind(this)} data-txn-type="OTP">One Time Payment</a></li>
                         </ul>
                      </div>
-                     <form class="mui-form">
-                        <legend>Provide EMI details</legend>
-                        <div class="mui-textfield">
-                            <input type="text" placeHolder="Amount"/>
-                            <label>EMI Amount</label>
-                        </div>
-                        <div class="mui-textfield">
-                            <input type="text" placeHolder="Months"/>
-                            <label>Number of months</label>
-                        </div>
-                        <div class="mui-select">
-                            <select>
-                                <option>Bank of America - Checking</option>
-                                <option>Chase Saphire Reserve - 5544</option>
-                                <option>Bank of America Cash Rewards Visa Signature - 6112</option>
-                                <option>Bank of America Travel Rewards Visa Signature - 6112</option>
-                                </select>
-                            <label>Select a bank account</label>
-                        </div>
-                       
-                        <button type="submit" class="mui-btn mui-btn--raised">Submit</button>
-                        </form>
+                     <form class="mui-form">   
+                        {this.state.txnType === `EMI`? <EMI />  :null}
+                        {this.state.txnType === `OTP`? <OTP />  :null}
+                        {this.state.txnType ? <Accounts /> : null }
+                        {this.state.txnType ? <button type="submit" class="mui-btn mui-btn--raised">Submit</button>: null }
+                    </form>
                 </div>
             </div>)
     }
