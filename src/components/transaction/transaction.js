d1b88257c4c795e67024891dfe40cc2ee27299af
Om Prakash Kannusami <okannusami@ebay.com>
2018-10-20 15:44:42 -0700

    adding mui

--- src/components/transaction/transaction.js
+++ src/components/transaction/transaction.js
@@ -8,9 +8,33 @@ export default class Transaction extends React.Component {
 
     render(){
         return (
-            <div>
+            <div style={{"padding": "1rem"}}>
                 <Header />
-                Add Transaction
+                <div style={{"font-weight": "bold", "font-size": "16px"}}>Add a transaction</div>
+                <div>
+                    <div class="mui-dropdown">
+                        <button class="mui-btn mui-btn--primary" data-mui-toggle="dropdown">
+                            Type of payment
+                            <span class="mui-caret"></span>
+                        </button>
+                        <ul class="mui-dropdown__menu">
+                            <li><a href="javascript:;">EMI</a></li>
+                            <li><a href="javascript:;">One Time Payment</a></li>
+                        </ul>
+                     </div>
+                     <form class="mui-form">
+                        <legend>Provide EMI details</legend>
+                        <div class="mui-textfield">
+                            <input type="text" placeHolder="Amount"/>
+                            <label>EMI Amount</label>
+                        </div>
+                        <div class="mui-textfield">
+                            <input type="text" placeHolder="Months"/>
+                            <label>Number of months</label>
+                        </div>
+                        <button type="submit" class="mui-btn mui-btn--raised">Submit</button>
+                        </form>
+                </div>
             </div>)
     }
 }
