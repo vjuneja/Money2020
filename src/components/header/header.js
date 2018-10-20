aff1f6e96114849aadc703c7192c6ce51c7b519e
Om Prakash Kannusami <okannusami@ebay.com>
2018-10-20 16:18:48 -0700

    adding header and emi component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 
 const Header = ()=> {
-    return <div>Equalizer</div>
+    return <div className="mui-appbar">
+        <table width="100%">
+            <tr style={{"vertical-align": "middle"}}>
+            <td className="mui--appbar-height" style={{"font-size": "18px", "font-weight": "bold"}}>Equalizer</td>
+            <td className="mui--appbar-height" align="right">Right Side</td>
+            </tr>
+        </table>
+    </div>
 }
 
 export default Header
